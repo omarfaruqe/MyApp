9193ee943bd244277ff3119f4f3385a4861f1dfb
farhamahzabin <farha.rucse@gmail.com>
2021-12-13 13:00:57 +0600

    change text

--- app/src/main/java/bd/ac/ru/cse/MainActivity.kt
+++ app/src/main/java/bd/ac/ru/cse/MainActivity.kt
@@ -2,10 +2,12 @@ package bd.ac.ru.cse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
