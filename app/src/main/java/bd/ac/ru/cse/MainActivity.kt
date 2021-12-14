684c692a77cb0d4adbbf8e0c3118a5890ca5ced0
Nahid-Hassan <nahid.cseru@gmail.com>
2021-12-14 16:58:25 +0600

    add gps service

--- app/src/main/java/bd/ac/ru/cse/MainActivity.kt
+++ app/src/main/java/bd/ac/ru/cse/MainActivity.kt
@@ -1,30 +1,140 @@
 package bd.ac.ru.cse
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.LocationManager
+import android.location.LocationRequest
 import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import org.json.JSONObject
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.jar.Manifest
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
 
-    val CITY: String = "dhaka,bd"
+    var CITY: String = ""
+//    val API:String = ""
     val API: String = "8118ed6ee68db2debfaaa5a44c832918" // Use your own API key
+    val PERMISSION_ID = 43
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        weatherTask().execute()
-
+        // initiate the fused....provider client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this )
+        getLastLocation()
     }
 
+    // get the last location
+    private fun getLastLocation() {
+        var cityName:String = ""
+        var countryCode:String = ""
+
+        // first we check permission
+        if (checkPerminssion()) {
+            if (isLocationEnabled()) {
+                // lets get the location
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener{ task->
+                    var location= task.result
+                    if (location != null) {
+                        val lat = location.latitude
+                        val long = location.longitude
+
+                        var geocoder = Geocoder(this, Locale.getDefault())
+                        var address = geocoder.getFromLocation(lat, long, 3)
+
+                        cityName = address.get(0).locality
+                        countryCode = address.get(0).countryCode
+
+                        Log.d("debug", cityName)
+                        Log.d("debug", countryCode)
+                        CITY = "$cityName,$countryCode"
+                        Log.d("debug", CITY)
+
+                        weatherTask().execute()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please enable your location service.", Toast.LENGTH_SHORT)
+            }
+        } else {
+            requestPermission()
+        }
+     }
+
+    // GPS
+    // Check the user permission
+    private fun checkPerminssion():Boolean{
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == PERMISSION_ID) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Debug", "You Have the permission")
+            }
+        }
+    }
+
+    // allows us to get user permission
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_ID)
+    }
+
+    // Is location service is enabled or not??
+    private fun isLocationEnabled():Boolean {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+
+    // Interface
     inner class weatherTask() : AsyncTask<String, Void, String>() {
         override fun onPreExecute() {
             super.onPreExecute()
